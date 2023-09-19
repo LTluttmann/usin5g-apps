408a44c8befd9a64ac3c8a03f3c0e869b94e19f6
Laurin Luttmann <laurin.luttmann@gmail.com>
2023-09-19 14:14:38 +0200

    init commit

---
+++ transport_sim/src/sim.py
@@ -0,0 +1,159 @@
+import logging
+import tb_rest_client.rest_client_pe as tbr
+from datetime import datetime
+import numpy as np
+from dataclasses import dataclass
+import time
+from config import (
+    USERS, 
+    ROBO_ID, 
+    URL, 
+    NEXT_TASK_ID,
+    HOME_COORD
+) 
+
+
+tenant = USERS["tenant"]
+tenant_client = tbr.RestClientPE(base_url=URL)
+tenant_client.login(username=tenant["mail"], password=tenant["pw"])
+
+
+@dataclass
+class Coordinates:
+    long: float
+    lat: float
+
+    def numpy(self):
+        return np.array([self.long, self.lat])
+    
+    def __eq__(self, __value: object) -> bool:
+        try:
+            return np.all(self.numpy() == __value.numpy())
+        except AttributeError:
+            raise ValueError("cannot compare object of type %s with Coordinates" % type(__value))
+
+
+@dataclass
+class PickingRoboter:
+    coord: Coordinates
+    battery: float = 100
+    num_tasks: int = 3
+    temperature: float = 54.0
+    speed: float = 0.0002
+
+
+@dataclass
+class PickingTask:
+    coord: Coordinates = Coordinates(10.4034710, 53.2280540)
+
+
+
+
+def get_latest_val(entity_id: str, entity_type: str = "DEVICE", key="speed", dtype=float):
+    latest = tenant_client.telemetry_controller.get_latest_timeseries_using_get(
+        entity_type, 
+        entity_id, 
+        keys=key
+    )
+    latest = dtype(latest[key][0]["value"])
+    return latest
+
+
+
+def update_task(task: PickingTask):
+    _ = tenant_client.telemetry_controller.save_entity_telemetry_using_post(
+        entity_type="ASSET",
+        entity_id=NEXT_TASK_ID,
+        scope="a",
+        body={
+            "longitude": task.coord.long,
+            "latitude": task.coord.lat
+        }
+    )
+
+
+def update_robo(robo: PickingRoboter):
+    _ = tenant_client.telemetry_controller.save_entity_telemetry_using_post(
+        entity_type="DEVICE",
+        entity_id=ROBO_ID,
+        scope="a",
+        body={
+            "longitude": robo.coord.long,
+            "latitude": robo.coord.lat,
+            "temperature": robo.temperature,
+            "battery": robo.battery,
+            "num_tasks": robo.num_tasks
+        }
+    )
+
+
+def move_robo_to_task(robo: PickingRoboter, task: PickingTask):
+    speed = get_latest_val(ROBO_ID, "DEVICE", "speed", float)
+    robo.speed = speed
+    
+    diff = task.coord.numpy() - robo.coord.numpy()
+    norm_diff = np.linalg.norm(diff)
+    if norm_diff <= speed:
+        robo.coord = task.coord
+    else:
+        norm = (diff / norm_diff) * speed
+        robo.coord = Coordinates(*list(robo.coord.numpy() + norm))
+
+    return robo
+
+
+def load_battery(robo: PickingRoboter):
+    CHARGE_RATE = 10.4
+    while not robo.battery >= 100:
+        robo.battery += CHARGE_RATE
+        robo.battery = min(robo.battery, 100)
+        update_robo(robo)
+        time.sleep(1.0)
+    return robo
+
+def get_random_task(depot: Coordinates):
+    add_coord = np.random.normal(0, 0.002, (2,))
+    add_coord = np.clip(add_coord, -0.004, 0.004)
+    new_coord = Coordinates(*list(depot.numpy() + add_coord))
+    task = PickingTask(new_coord)
+    return task
+
+
+def main():
+
+    depot = Coordinates(*HOME_COORD)
+    robo = PickingRoboter(depot)
+    task = get_random_task(depot)
+    
+    while True:
+
+        if np.random.random() < 0.04:
+            robo.num_tasks += 1
+
+
+        if robo.num_tasks == 0:
+            time.sleep(1.0)
+            continue
+        else:
+            update_task(task)
+
+        robo.battery -= 1.4
+        robo = move_robo_to_task(robo, task)
+
+        if robo.coord == task.coord:
+
+            if task.coord == depot:
+                robo = load_battery(robo)
+            else:
+                robo.num_tasks -= 1
+            task = get_random_task(depot)
+
+        if robo.battery <= 30.0:
+            task = PickingTask(HOME_COORD)
+
+        update_robo(robo)
+        time.sleep(1.0)
+
+
+if __name__ == "__main__":
+    main()
