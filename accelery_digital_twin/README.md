5c269ff3c6cd719f00a98f2d44b53a762358a853
Zaponex <ole.pruefer@leuphana.de>
2024-04-12 12:03:44 +0200

    Add Accelery digital twin

---
+++ accelery_digital_twin/README.md
@@ -0,0 +1,4 @@
+The digital twin illustrates the accelery shopfloor in 3D build with three.js library. Live messages can be displayed over enabled mqtt with websockets plugin of rabbitmq. The goal is an real time data integrated animated visualization of several production parts using the 5G private network for data streaming. The long term goal is the implemention in thingsboards iot platform as an central webbased control station.
+
+
+![](/src/img/accelery_digital_twin.png)
---
+++ accelery_digital_twin/src/js/mqttClient.js
@@ -0,0 +1,62 @@
+// Assuming 'mqtt' is imported or available globally
+let mqttClient;
+let latestMessage = "Standardnachricht";
+
+function connectToBroker() {
+    const clientId = "client" + Math.random().toString(36).substring(7);
+    const host = "ws://4.184.199.231:15675/ws"; // Use "wss://" for secure connections
+    const options = {
+        keepalive: 60,
+        clientId,
+        protocolId: "MQTT",
+        protocolVersion: 4,
+        clean: true,
+        reconnectPeriod: 1000,
+        connectTimeout: 30 * 1000,
+        username: "prueferol",
+        password: "Susauskaka27!",
+    };
+
+    mqttClient = mqtt.connect(host, options);
+
+    mqttClient.on("connect", () => {
+        console.log("Client connected:" + clientId);
+        // Example: Subscribe directly upon successful connection
+        subscribeToTopic("sensors");
+    });
+
+    mqttClient.on("message", (topic, message, packet) => {
+        latestMessage = message.toString();
+        console.log(`Received Message: ${latestMessage} On topic: ${topic}`);
+    });
+
+    mqttClient.on("error", (err) => {
+        console.log("Connection Error:", err);
+        mqttClient.end();
+    });
+
+    mqttClient.on("reconnect", () => {
+        console.log("Reconnecting...");
+    });
+}
+
+function subscribeToTopic(topic) {
+    if (!mqttClient) {
+        console.log("MQTT client not initialized");
+        return;
+    }
+    mqttClient.subscribe(topic, { qos: 0 }, (error) => {
+        if (error) {
+            console.log("Subscribe error:", error);
+        } else {
+            console.log("Subscribed to:", topic);
+        }
+    });
+}
+
+function getLatestMessage() {
+    return latestMessage;
+}
+connectToBroker();
+export { getLatestMessage };
+
